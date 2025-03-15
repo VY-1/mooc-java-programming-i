0100595d695c51294f4e20bb6bccc01c2eb9803f
VY <veasna.youphon@live.com>
2025-03-15 16:07:14 -0700

    complete 4-24

--- part04-Part04_24.PrintingAFile/src/main/java/PrintingAFile.java
+++ part04-Part04_24.PrintingAFile/src/main/java/PrintingAFile.java
@@ -5,17 +5,15 @@ import java.util.Scanner;
 public class PrintingAFile {
 
     public static void main(String[] args) {
-        try(Scanner scanner = new Scanner(Paths.get("data.txt"))){
 
-            //read the file until all lines have been read
+        try(Scanner scanner = new Scanner(Paths.get("data.txt"))){
             while(scanner.hasNextLine()){
-                //we read one line
                 String row = scanner.nextLine();
-                //we print the line that we read
                 System.out.println(row);
             }
         }catch(Exception e){
             System.out.println("Error: " + e.getMessage());
         }
+
     }
 }
