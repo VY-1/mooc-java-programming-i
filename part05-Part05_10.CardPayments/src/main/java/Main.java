dc81d76827a4a4ee919047f918a669fe6f463406
VY <veasna.youphon@live.com>
2025-03-17 21:08:17 -0700

    complete 5-10

---
+++ part05-Part05_10.CardPayments/src/main/java/Main.java
@@ -0,0 +1,49 @@
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write experimental main programs here
+
+        PaymentCard petesCard = new PaymentCard(10);
+        System.out.println("money " + petesCard.balance());
+        boolean wasSuccessful = petesCard.takeMoney(8);
+        System.out.println("successfully withdrew: " + wasSuccessful);
+        System.out.println("money " + petesCard.balance());
+
+        wasSuccessful = petesCard.takeMoney(4);
+        System.out.println("successfully withdrew: " + wasSuccessful);
+        System.out.println("money " + petesCard.balance());
+
+        //Using paymentterminal class
+        PaymentTerminal unicafeExactum = new PaymentTerminal();
+
+        double change = unicafeExactum.eatAffordably(10);
+        System.out.println("remaining change " + change);
+
+        change = unicafeExactum.eatAffordably(5);
+        System.out.println("remaining change " + change);
+
+        change = unicafeExactum.eatHeartily(4.3);
+        System.out.println("remaining change " + change);
+
+        System.out.println(unicafeExactum);
+
+        //Using paymentTerminal and paymentcard
+        PaymentCard annesCard = new PaymentCard(2);
+
+        System.out.println("amount of money on the card is " + annesCard.balance() + " euros");
+
+        wasSuccessful = unicafeExactum.eatHeartily(annesCard);
+        System.out.println("there was enough money: " + wasSuccessful);
+
+        unicafeExactum.addMoneyToCard(annesCard, 100);
+
+        wasSuccessful = unicafeExactum.eatHeartily(annesCard);
+        System.out.println("there was enough money: " + wasSuccessful);
+
+        System.out.println("amount of money on the card is " + annesCard.balance() + " euros");
+
+        System.out.println(unicafeExactum);
+    }
+}
+
